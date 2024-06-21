6e7cb714dc2b235792511860cceb1b7c21ec016a
Andrii Karpenko <57965389+TENOVOD@users.noreply.github.com>
2024-06-21 11:46:50 +0300

    added padding if user use a navigation bar (added to backgrounds)

--- app/src/main/java/com/example/pulselight/ui/backgrounds/BackgroundForHomeAndHistoryPages.kt
+++ app/src/main/java/com/example/pulselight/ui/backgrounds/BackgroundForHomeAndHistoryPages.kt
@@ -3,13 +3,18 @@ package com.example.pulselight.ui.backgrounds
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.pulselight.ui.theme.BgColor
 import com.example.pulselight.ui.theme.ElipseBG
 
@@ -19,6 +24,7 @@ fun HomepageBackground(content: @Composable () -> Unit){
         modifier = Modifier
             .fillMaxSize()
             .background(BgColor) // Білий фон для всього екрану
+            .padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding())
     ) {
         HomepageCircle()
         content()
--- app/src/main/java/com/example/pulselight/ui/backgrounds/BackgroundWithElipse.kt
+++ app/src/main/java/com/example/pulselight/ui/backgrounds/BackgroundWithElipse.kt
@@ -4,10 +4,13 @@ import android.util.DisplayMetrics
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -34,6 +37,7 @@ fun BackgroundWithCircle(content: @Composable () -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .background(BgColor) // Білий фон для всього екрану
+            .padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()+16.dp)
     ) {
         Circle()
         content()
